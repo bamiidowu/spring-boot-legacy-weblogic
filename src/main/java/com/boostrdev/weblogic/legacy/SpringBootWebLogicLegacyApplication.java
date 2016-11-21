7a68d056dc4c10266ab526d378e4fc7366a28314
Bami Idowu <bami.idowu@yahoo.co.uk>
2016-11-21 16:03:06 +0000

    Added comments and optimised POM

--- src/main/java/com/boostrdev/weblogic/legacy/SpringBootWebLogicLegacyApplication.java
+++ src/main/java/com/boostrdev/weblogic/legacy/SpringBootWebLogicLegacyApplication.java
@@ -6,14 +6,10 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.WebApplicationInitializer;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 //Tell Spring to automatically inject any dependencies that are marked in
 //our classes with @Autowired
 @EnableAutoConfiguration
-// Tell Spring to turn on WebMVC (e.g., it should enable the DispatcherServlet
-// so that requests can be routed to our Controllers)
-@EnableWebMvc
 // Tell Spring that this object represents a Configuration for the
 // application
 @Configuration
